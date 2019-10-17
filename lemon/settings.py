253d3f3d9609204cfb0b45c1dbdd194cb32c3771
joaxin <lotility@gmail.com>
2019-10-17 14:20:54 +0800

    heroku deploy2

--- lemon/settings.py
+++ lemon/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 # Application definition
 
 INSTALLED_APPS = [
-    'simpleui',
+     # 'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
